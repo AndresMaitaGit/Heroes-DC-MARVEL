f9d48785c5bc563bebfdb4d47e988105a2d95e7b
Luis Andres <69315128+AndresMaitaGit@users.noreply.github.com>
2020-08-18 23:41:07 -0400

    Update precache-manifest.2cf2c96614bd7fb703e1600029a87f46.js

--- docs/precache-manifest.2cf2c96614bd7fb703e1600029a87f46.js
+++ docs/precache-manifest.2cf2c96614bd7fb703e1600029a87f46.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "732418d641ba5ab4a921648230147907",
-    "url": "./index.html"
+    "url": "/index.html"
   },
   {
     "revision": "769cef536e7f5b9981d5",
-    "url": "./static/js/2.b5b54fcd.chunk.js"
+    "url": "/static/js/2.b5b54fcd.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "./static/js/2.b5b54fcd.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.b5b54fcd.chunk.js.LICENSE.txt"
   },
   {
     "revision": "b6e02bdc651f435bbd84",
-    "url": "./static/js/main.403e5210.chunk.js"
+    "url": "/static/js/main.403e5210.chunk.js"
   },
   {
     "revision": "94d7ca25d4df19621824",
-    "url": "./static/js/runtime-main.b7f813af.js"
+    "url": "/static/js/runtime-main.b7f813af.js"
   }
 ]);
